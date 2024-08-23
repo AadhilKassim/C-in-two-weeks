35957980520eb67309cd43cfcb6d21ee3ae2c792
Aadhil Kassim <aadhikassim@gmail.com>
2024-08-23 21:22:34 +0530

    Day1 Topics all covered

--- Day1/Main.c
+++ Day1/Main.c
@@ -2,6 +2,12 @@
 
 int main(void)
 {
-    printf("Hello world!");
+    int age =18;
+    char name[] = "Aadhil" ;
+    float weight= 34.02;
+    double marks = 93.16666666666666667 ;
+    printf("Hello world!\nMy first C program\n");
+    //to print double and float use %f or %lf as they are both essentially the same datatye with different precision
+    printf("Hi my name is %s. I'm %i years old. I weigh %fkg. My persent for 12th was %f",name,age,weight,marks);
     return 0;
 }
