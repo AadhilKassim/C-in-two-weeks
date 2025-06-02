d01d6d4a59fd0383572747bd582b5d33689d1ebb
Aadhil Kassim <aadhikassim@gmail.com>
2025-06-02 11:48:53 +0530

    Implemented quicksort
    Was very different from the other sort algorithms

--- Week 2/Day10/sort.c
+++ Week 2/Day10/sort.c
@@ -3,7 +3,8 @@
 void bubblesort(int *,int);
 void selectionsort(int *, int);
 void quicksort(int *, int);
-
+int partition(int *, int, int);
+void QuickSort(int *, int, int);
 
 int main()
 {
@@ -89,4 +90,58 @@ void selectionsort(int arr[],int size)
 void quicksort(int arr[],int size)
 {
     int data[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        data[i] = arr[i];
+    }
+    QuickSort(data, 0, size);
+    printf("Quick Sort:\t");
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%d\t",data[i]);
+    }
+    
+}
+
+
+
+int partition(int data[], int l, int h)
+{
+    int pivot = data[l];
+    int i = l, j = h;
+    
+    while (i<j)
+    {
+        do
+        {
+            i++;
+        } while (data[i]<=pivot);
+        do
+        {
+            j--;
+        } while (data[j]>pivot);
+        if (i<j)
+        {
+            int temp = data[j];
+            data[j] = data[i];
+            data[i] = temp;
+        }
+    }
+    int temp = data[j];
+        data[j] = data[l];
+        data[l] = temp;
+    return j ;
+}
+
+void QuickSort(int data[],int l, int h)
+{
+    int j;
+    if (l<h)
+    {
+        j = partition(data, l, h);
+        QuickSort(data, l, j);
+        QuickSort(data, j+1, h);
+        
+    }
+    
 }
